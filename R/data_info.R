b98a85f9972628b84a4aa4152172995db07ade23
Jeniah Franco <jfranco@smith.edu>
2023-12-07 11:08:43 -0500

    working on the data description

--- R/data_info.R
+++ R/data_info.R
@@ -3,17 +3,12 @@
 #' @format A data frame with 36 rows and 5 columns:
 #' \describe{
 #'   \item{year}{Year of fatalities}
-#'   \item{fatalities}{number of fatalities}
-#'   \item{city}{Month comic was published}
-#'   \item{day}{Day of month comic was published}
-#'   \item{link}{Link to relevant context, source information, or large version of the comic}
-#'   \item{news}{Seemingly random information about the comic (backstory, contributors, merchandise. May contain raw HTML.}
-#'   \item{title}{Title of the comic}
-#'   \item{safe_title}{Year}
-#'   \item{transcript}{Transcription of the text in the comic}
-#'   \item{alt}{alt text for image in web browsers}
-#'   \item{img}{URL for the comic image}
+#'   \item{total_fatal}{number of documented fatalities per year}
+#'   \item{city}{City where fatality took place}
+#'   \item{pop_size}{Population size for city as of 2022}
+#'   \item{fatal_perc}{Proportion of the population involved in fatalities per year}
+
 #'   ...
 #' }
-#' @source <https://xkcd.com/json.html>
-"xkcd_data"
+#' @source <I dont know what to put here >
+"vz_data_2023"
