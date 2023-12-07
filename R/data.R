860ee863b9ef710e375372eceee6b480b6e5e2d1
Janeth Covarrubias <jcovarrubiasperez@smith.edu>
2023-12-07 15:43:43 -0500

    documenting for DATASET.R

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Metadata for vehicle fatalities in Boston, NYC, Denver, Richmond, and Boulder ranging from years 2008-2023.
+#'
+#' @format A data frame with 36 rows and 5 columns:
+#' \describe{
+#'   \item{year}{year when fatalities occurred}
+#'   \item{total_fatal}{total number of fatalities in a given year}
+#'   \item{city}{city where fatalities took place}
+#'   \item{pop_size}{number of population in a given city}
+#'   \item{fatal_perc}{percentage of fatalities per capita}
+#'   ...
+#' }
+#' @sources <https://www.virginiaroads.org/datasets/VDOT::crashdata-details-3.csv,
+#' https://opendata.arcgis.com/datasets/4918642e10c84c7fbb8d8590c5b9b216_0.csv,
+#' https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv?date=20231129&accessType=DOWNLOAD,
+#' https://www.denvergov.org/media/gis/DataCatalog/traffic_accidents/csv/traffic_accidents.csv,
+#' https://data.boston.gov/dataset/d326a4e3-75f2-42ac-9b32-e2920566d04c/resource/92f18923-d4ec-4c17-9405-4e0da63e1d6c/download/tmp1u21fr8k.csv,
+#' *** url for populations?>
+"DATASET"
