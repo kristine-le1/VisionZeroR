9dc9a1e9ee3a4411c9619f368d40cc54b6f80ad7
Jeniah Franco <jfranco@smith.edu>
2023-12-08 15:45:34 -0500

    Completed data docum

--- R/data_info.R
+++ R/data_info.R
@@ -1,14 +1,21 @@
-#' Metadata for all xkcd comics (so far)
+#' Metadata for the our vision zero and population data
 #'
 #' @format A data frame with 36 rows and 5 columns:
 #' \describe{
 #'   \item{year}{Year of fatalities}
 #'   \item{total_fatal}{number of documented fatalities per year}
 #'   \item{city}{City where fatality took place}
-#'   \item{pop_size}{Population size for city as of 2022}
+#'   \item{pop_size}{Populationhelp size for city as of 2022}
 #'   \item{fatal_perc}{Proportion of the population involved in fatalities per year}
 
 #'   ...
 #' }
-#' @source <I dont know what to put here >
+#' @source
+#' <https://data.boston.gov/dataset/d326a4e3-75f2-42ac-9b32-e2920566d04c/resource/92f18923-d4ec-4c17-9405-4e0da63e1d6c/download/tmp1u21fr8k.csv>
+#' <https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv?date=20231129&accessType=DOWNLOAD>
+#' <https://www.denvergov.org/media/gis/DataCatalog/traffic_accidents/csv/traffic_accidents.csv>
+#' <https://opendata.arcgis.com/datasets/4918642e10c84c7fbb8d8590c5b9b216_0.csv>
+#' https://www.virginiaroads.org/datasets/VDOT::crashdata-details-3.csv
+#' <https://www.census.gov/quickfacts/fact/csv/bostoncitymassachusetts,richmondcityvirginia,denvercountycolorado,bouldercitycolorado,newyorkcitynewyork/PST045222>
+
 "vz_data_2023"
