fb70e339100b8dd659c4c60db7487aa4fef8223a
Kristine Le <kle@smith.edu>
2023-12-11 15:26:58 -0500

    Fixed grammar mistakes in helpfile

--- R/Function.R
+++ R/Function.R
@@ -1,10 +1,10 @@
 #' Creates point graph of vehicular fatalities normalized to the population size for user-given VisionZero cities
 #'
-#' Given 1 to 5 VisionZero cities (Boston, NYC, Boulder, Denver or Richmond), this function creates a point graph of vehicular fatalities throughout the years (from 2008-2023) normalized to the population size of the city for the user-given cities. This function uses vehicular fatality and population data aggregated for this cities in the `vz_data_2023` data set
+#' Given 1 to 5 VisionZero cities (Boston, NYC, Boulder, Denver or Richmond), this function creates a point graph of vehicular fatalities throughout the years (from 2008-2023) normalized to the population size of the city for the user-given cities. This function uses vehicular fatality and population data aggregated for these cities in the `vz_data_2023` data set
 #'
 #' @param character A character vector of up to 5 elements containing city names from five different VisionZero cities: Boston, NYC, Boulder, Denver, and Richmond.
 #'
-#' @return A graph in the `plots` window looking at the percentage of vehicular fatalities based on population size across the years 2008-2023 (year range dependent on the available data for the user-given cities).
+#' @return A point graph in the `plots` window displaying the percentage of vehicular fatalities based on population size across the years 2008-2023 (year range dependent on the available data for the user-given cities).
 #'
 #' @examples
 #' # Graphing vehicular fatality data for just Richmond
