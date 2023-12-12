59057a7c74538ee20f7a116a2a6d229797048b77
Janeth Covarrubias <jcovarrubiasperez@smith.edu>
2023-12-12 09:41:05 -0500

    test

--- tests/testthat.R
+++ tests/testthat.R
@@ -11,38 +11,3 @@ library(VisionZeroR)
 
 test_check("VisionZeroR")
 
-library(testthat)
-library(ggplot2)
-
-test_that("Graphing Test", {
-  vz_data_2023 <- data.frame(
-    city = c("Boston", "Denver", "Boulder", "NYC", "Richmond"),
-    year = rep(2008:2023, each = 4),
-    fatal_perc = runif(20)
-  )
-
-  test_plot <- vz_graph(c("Boston", "Denver", "Boulder", "NYC", "Richmond"))
-
-  expected_plot <-   ggplot(data = new,
-                            aes(x = year,
-                                y = fatal_perc,
-                                color = city,
-                                group = city)) +
-    geom_point() +
-    geom_line() +
-    labs(title = "Normalized vehicular fatalities per year for VisionZero cities",
-         x = "Year",
-         y = "Normalized fatalities by percent of population",
-         color = "City")
-
-  #actual plot vs expected plot
-  expect_identical(test_plot, expected_plot)
-})
-
-test_that("VisionZeroR objects have 5 elements", {
-  expect_equal(length(VisionZeroR(3)), 5)
-})
-
-test_that("VisionZeroR objects are type character", {
-  expect_true(is.character(VisionZeroR(3)))
-})
---
+++ tests/testthat/test-Function.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(VisionZeroR)
+
+library(vdiffr)
+
+  test_that("Graphing test", {
+    vdiffr::expect_doppelganger(
+      title = "vz_graph",
+      fig = vz_graph(c("Boston", "Denver", "Boulder", "NYC", "Richmond"))
+    )
+})
