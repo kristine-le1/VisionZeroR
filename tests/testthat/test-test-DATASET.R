c003c2b65b2de155d8f2d3e5e19573ee1c41e04a
Janeth Covarrubias <jcovarrubiasperez@smith.edu>
2023-12-14 23:12:43 -0500

    test-DATASET.R

---
+++ tests/testthat/test-test-DATASET.R
@@ -0,0 +1,9 @@
+test_that("vz_data_2023 row count", {
+  expect_equal(nrow(vz_data_2023), 45)
+})
+test_that("vz_data_2023 column count", {
+  expect_equal(ncol(vz_data_2023), 5)
+})
+test_that("variable search", {
+  expect_equal(names(vz_data_2023), c("year", "total_fatal", "city", "pop_size", "fatal_perc"))
+})
