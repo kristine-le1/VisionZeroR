47d7a60ddbd57b56dfa393ca0228d1f55280795e
Janeth Covarrubias <jcovarrubiasperez@smith.edu>
2023-12-14 23:28:25 -0500

    FIXING EXPECTED VALUE

--- tests/testthat/test-test-DATASET.R
+++ tests/testthat/test-test-DATASET.R
@@ -1,5 +1,5 @@
 test_that("vz_data_2023 row count", {
-  expect_equal(nrow(vz_data_2023), 45)
+  expect_equal(nrow(vz_data_2023), 36)
 })
 test_that("vz_data_2023 column count", {
   expect_equal(ncol(vz_data_2023), 5)
